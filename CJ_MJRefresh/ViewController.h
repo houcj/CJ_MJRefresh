378487cfa9a042b4cd64b6a79cc808107303bed0
侯春娇 <houchunjiao@houchunjiaodeMacBook-Pro.local>
2017-07-04 10:46:37 +0800

    Initial Commit

---
+++ CJ_MJRefresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJ_MJRefresh
+//
+//  Created by 侯春娇 on 2017/7/4.
+//  Copyright © 2017年 侯春娇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CJ_MJRefreshTests/CJ_MJRefreshTests.m
@@ -0,0 +1,39 @@
+//
+//  CJ_MJRefreshTests.m
+//  CJ_MJRefreshTests
+//
+//  Created by 侯春娇 on 2017/7/4.
+//  Copyright © 2017年 侯春娇. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CJ_MJRefreshTests : XCTestCase
+
+@end
+
+@implementation CJ_MJRefreshTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
